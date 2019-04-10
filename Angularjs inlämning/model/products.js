dcaae1f86f9ed2458895c779da59711ef2a40992
berglundaugust <46716816+berglundaugust@users.noreply.github.com>
2019-04-10 13:31:16 +0200

    Add files via upload

---
+++ Angularjs inlämning/model/products.js
@@ -0,0 +1,12 @@
+var products = [
+    { vendor: "Crecent", productname: "MTB", category: "MTB", shortdescription: "Metallic", price: 2900 , instock : 20, instockdate: new Date("2019-03-05")  },
+    { vendor: "Crecent", productname: "City cruiser", category: "Bikes", shortdescription: "Blue", price: 2900 , instock : 1, instockdate: new Date("2019-03-04")  },
+    { vendor: "Crecent", productname: "Three wheeler", category: "Bikes", shortdescription: "Gray", price: 2900 , instock : 14, instockdate: new Date("2019-03-01")  },
+    { vendor: "Scott", productname: "MTB", category: "MTB", shortdescription: "Black", price: 4000 , instock : 12, instockdate: new Date("2019-03-02")  },
+    { vendor: "Scott", productname: "City cruiser", category: "Bikes", shortdescription: "Yellow", price: 4000 , instock : 44, instockdate: new Date("2019-03-02")  },
+    { vendor: "Scott", productname: "Three wheeler", category: "Bikes", shortdescription: "Red", price: 4000 , instock : 2, instockdate: new Date("2019-03-01")  },
+    { vendor: "Scott", productname: "MTB", category: "MTB", shortdescription: "Brown", price: 4000 , instock : 3, instockdate: new Date("2019-03-04")  },
+    { vendor: "Merida", productname: "MTB", category: "MTB", shortdescription: "Spacegray", price: 6000 , instock :20, instockdate: new Date("2019-03-02")  },
+    { vendor: "Merida", productname: "City Cruiser", category: "Bikes", shortdescription: "Metallic red", price: 6000 , instock : 20, instockdate: new Date("2019-03-05")  },
+    { vendor: "Merida", productname: "Three wheeler", category: "Bikes", shortdescription: "Pink", price: 6000 , instock : 20, instockdate: new Date("2019-03-01")  }
+];
